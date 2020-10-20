8f1a18a044860449695cb8954194f39960e170fd
Supriya <supriyagalbale@gmail.com>
2020-10-20 20:19:45 +0530

    diapatching store

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,10 +8,10 @@ class MovieCard extends React.Component {
         const { movie } = this.props;
         return (
             <div className="movie-card">
-              <div classname="left">
+              <div className="left">
                   <img alt="movie-poster" src={movie.Poster}/>
               </div>
-              <div classname="right">
+              <div className="right">
                   <div className="title">{movie.Title}</div>
                   <div className="plot">{movie.Plot}</div>
                   <div className="footer">
--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ import movies from './reducers';
 
 const store = createStore(movies);
 console.log('store', store);
-console.log('STATE' , store.getState());
+console.log('BEFORE_STATE' , store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{name: 'Superman' }]
+});  
+
+console.log('AFTER_STATE' , store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
